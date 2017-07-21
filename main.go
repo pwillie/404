6a87ecb75cdeba3bc0e450f41346b9dccc0ca545
Peter Wilson <pwillie@users.noreply.github.com>
2017-07-21 17:33:59 +1000

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func healthcheck_handler(res http.ResponseWriter, req *http.Request) {
@@ -17,5 +22,28 @@ func default_handler(res http.ResponseWriter, req *http.Request) {
 func main() {
 	http.HandleFunc("/status", healthcheck_handler)
 	http.HandleFunc("/", default_handler)
-	http.ListenAndServe(":8080", nil)
+
+	srv := &http.Server{
+		Handler: nil,
+		Addr:    ":8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		log.Println()
+		log.Printf("Received signal: %v", sig)
+		log.Println("Shutting down...")
+		ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+		srv.Shutdown(ctx)
+	}()
+
+	log.Println("Listening on addr: :8080")
+	log.Fatal(srv.ListenAndServe())
 }
